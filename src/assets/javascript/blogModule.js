52725e72d7d093d55ecf6b051788316160149564
negm <negm@afaqy.com>
2016-10-06 02:24:41 +0300

    Construct the basic Layout

---
+++ src/assets/javascript/blogModule.js
@@ -0,0 +1,20 @@
+/**
+ * Created by negm on 06/10/16.
+ */
+
+var blogModule = (function () {
+
+    function addActions() {
+        //To have the Events of elements
+    }
+
+    return {
+        init: function () {
+            addActions();
+        }
+    }
+})();
+
+$(document).ready(function () {
+    blogModule.init();
+});
