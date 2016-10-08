8c74af43f672b73fd5d299a51880ef0ce6455153
negm <negm@afaqy.com>
2016-10-08 10:50:13 +0300

    Add New Post, Update Exist Post, Delete Post and Check Errors for whole APIs

--- src/assets/javascript/blogModule.js
+++ src/assets/javascript/blogModule.js
@@ -2,9 +2,10 @@
  * Created by negm on 06/10/16.
  */
 
-var blogModule = (function () {
+var postModule = (function () {
 
-    var blogId = -1; //-1 undefined, 0 new, else id of post
+    var postId = -1; //-1 undefined, 0 new, else id of post
+
     function fillUserDropdownList(callback) {
         $.get('/api/users', function (data) {
             $("#userInput").html('<option value="">Select User</option>');
@@ -27,6 +28,88 @@ var blogModule = (function () {
             if ($.isFunction(callback)) {
                 callback(post);
             }
+        }).fail(function (xhr) {
+            var response = JSON.parse(xhr.responseText);
+            if (response.error) {
+                alert(response.error);
+            } else {
+                alert("Post view has error, Try again later");
+            }
+        });
+    }
+
+    function validPost(data) {
+        /**
+         * check the user is not Empty
+         * check the user is Numeric
+         * Check The post content is not Empty
+         * Check the post content is more than 100 characters
+         * then return and Object has the status and/or the Errors
+         */
+
+        var valid = true;
+        var errors = [];
+
+        if (data.user == null || data.user === '') {
+            valid = false;
+            errors.push("User must not be empty");
+        } else if (data.user != parseInt(data.user, 10)) {
+            valid = false;
+            errors.push("User is invalid");
+        }
+
+        if (data.content == null || data.content === '') {
+            valid = false;
+            errors.push("Content must not be empty");
+        } else {
+            var str = data.content.replace(/\s+/g, ' ');
+            if (str.length < 100) {
+                valid = false;
+                errors.push("Content length must be more than 100 characters, current length is " + str.length);
+            }
+        }
+
+        return {'status': valid, errors: errors};
+    }
+
+    function addNewPost(post, callback) {
+        $.post('/api/posts/new', post, function (response) {
+            if (response.msg == "OK") {
+                var id = response.id;
+                if ($.isFunction(callback)) {
+                    callback(id);
+                }
+            }
+        }).fail(function (xhr) {
+            var response = JSON.parse(xhr.responseText);
+            if (response.error) {
+                alert(response.error);
+            } else {
+                alert("Post insertion failed, Try again later");
+            }
+        });
+    }
+
+    function editPost(post, callback) {
+        $.ajax({
+            url: '/api/posts/edit/' + post.id,
+            type: 'PUT',
+            data: post,
+            success: function (response) {
+                if (response.msg == "OK") {
+                    var id = response.id;
+                    if ($.isFunction(callback)) {
+                        callback(id);
+                    }
+                }
+            }
+        }).fail(function (xhr) {
+            var response = JSON.parse(xhr.responseText);
+            if (response.error) {
+                alert(response.error);
+            } else {
+                alert("Post updating failed, Try again later");
+            }
         });
     }
 
@@ -34,7 +117,7 @@ var blogModule = (function () {
         //To have the Events of elements
 
         $('#controlPostModal').on('hidden.bs.modal', function () {
-            blogId = -1;
+            postId = -1;
         });
 
         $("#createPostBtn").on('click', function () {
@@ -46,7 +129,7 @@ var blogModule = (function () {
             fillUserDropdownList(function () {
                 $("#userInput").val("");
                 $("#contentInput").html("");
-                blogId = 0;
+                postId = 0;
 
                 $("#controlPostModal").modal('show');
             });
@@ -64,18 +147,13 @@ var blogModule = (function () {
                 getPostData(id, function (post) {
                     $("#userInput").val(post.user_id);
                     $("#contentInput").html(post.content);
-                    blogId = post.rowid;
-                    console.log(blogId);
+                    postId = post.rowid;
+                    console.log(postId);
 
                     $("#controlPostModal").modal('show');
                 });
 
             });
-
-            $("#userInput").val("");
-            $("#contentInput").html("");
-
-            $("#controlPostModal").modal('show');
         });
 
         $(".deletePostBtn").on('click', function () {
@@ -91,29 +169,86 @@ var blogModule = (function () {
                 var deleteBtn = $(this);
                 console.log(viewPage);
                 $.ajax({
-                    url: '/api/posts/delete/'+id,
+                    url: '/api/posts/delete/' + id,
                     type: 'DELETE',
-                    success: function(data) {
-                        if(data.msg=="OK") {
+                    success: function (data) {
+                        if (data.msg == "OK") {
                             alert("The Post Delete Successfully");
 
                             //if the deletion from View Page to redirect to the home page
-                            if(viewPage == 1){
+                            if (viewPage == 1) {
                                 location.href = "/";
-                            }else{
+                            } else {
                                 //if from any list page just remove it from the DOM
                                 var rowElement = deleteBtn.parent().parent().parent();
-                                rowElement.fadeOut(600, function(){
+                                rowElement.fadeOut(600, function () {
                                     $(this).remove();
                                 })
                             }
+                        } else {
+                            alert("Post deleting failed, Try again later");
                         }
 
                     }
-
+                }).fail(function (xhr) {
+                    var response = JSON.parse(xhr.responseText);
+                    if (response.error) {
+                        alert(response.error);
+                    } else {
+                        alert("Post deleting failed, Try again later");
+                    }
                 });
             }
 
+        });
+
+        $("#postSaveBtn").on('click', function () {
+            /**
+             * check if the post come from the modal by the right way
+             * collect the data from inputs
+             * check validations
+             * if it's a new post send then go to add post
+             * else it's an edition for exist post go to update it
+             * then alert the user by the results
+             * go to the post page
+             */
+
+            if (postId == -1) {
+                alert("There is some error, please try again later");
+                return false;
+            }
+
+            var post = {};
+            post.user = $("#userInput").val();
+            post.content = $("#contentInput").val();
+
+            var validation = validPost(post);
+            if (validation.status == false) {
+                var errorHTML = "";
+                $.each(validation.errors, function (i, error) {
+                    errorHTML += "<li>" + error + "</li>";
+                });
+
+                $("#postModalErrorsMsg ol").html(errorHTML);
+                $("#postModalErrorsMsg").show();
+            } else {
+                $("#postModalErrorsMsg").hide();
+
+                if (postId == 0) {
+                    //it's new Post
+                    addNewPost(post, function (id) {
+                        alert("Post has been created successfully");
+                        location.href = "/posts/" + id;
+                    });
+                } else if (postId != -1) {
+                    //it's edition for exist post;
+                    post.id = postId;
+                    editPost(post, function (id) {
+                        alert("Post has been edited successfully");
+                        location.href = "/posts/" + id;
+                    })
+                }
+            }
         });
     }
 
@@ -125,5 +260,5 @@ var blogModule = (function () {
 })();
 
 $(document).ready(function () {
-    blogModule.init();
+    postModule.init();
 });
--- src/index.php
+++ src/index.php
@@ -1,17 +1,19 @@
 <?php
 
 // Silex documentation: http://silex.sensiolabs.org/doc/
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 require_once __DIR__ . '/../vendor/autoload.php';
+
 
 $app = new Silex\Application();
 
 $app['debug'] = true;
 
-/* SQLite config
-
-TODO: Add a users table to sqlite db
-*/
+/**
+ * SQLite config
+ */
 
 $app->register(new Silex\Provider\DoctrineServiceProvider(), array(
     'db.options' => array(
@@ -27,18 +29,7 @@ $app->register(new Silex\Provider\TwigServiceProvider(), array(
 
 
 /* ------- micro-blog api ---------
-
 All CRUD operations performed within our /api/ endpoints below
-
-TODO: Error checking - e.g. if try retrieve posts for a user_id that does
-      not exist, return an error message and an appropriate HTTP status code.
-
-      Implement /api/posts/new endpoint to add a new micro-blog post for a
-      given user.
-
-      Extra: Add new API endpoints for any extra features you can think of.
-
-      Extra: Improve on current API code where you see necessary
 */
 
 $app->get('/api/posts', function () use ($app) {
@@ -53,7 +44,7 @@ $app->get('/api/posts/user/{user_id}', function ($user_id) use ($app) {
     $sql = "SELECT user_id FROM users WHERE user_id = ?";
     $user = $app['db']->fetchAssoc($sql, array((int)$user_id));
     if (!$user) {
-        $app->abort(404, "User $user_id does not exist.");
+        return $app->json(array('msg' => 'FAIL', 'error' => "User $user_id does not exist."), 404);
     }
 
     $sql = "SELECT posts.rowid, posts.*, users.name FROM posts INNER JOIN users ON users.user_id = posts.user_id WHERE posts.user_id = ?";
@@ -67,21 +58,85 @@ $app->get('/api/posts/id/{post_id}', function ($post_id) use ($app) {
     $post = $app['db']->fetchAssoc($sql, array((int)$post_id));
 
     if (!$post) {
-        $app->abort(404, "Post $post_id does not exist.");
+        return $app->json(array('msg' => 'FAIL', 'error' => "Post $post_id does not exist."), 404);
     }
     return $app->json($post, 200);
 });
 
-$app->post('/api/posts/new', function (Request $request) {
+$app->post('/api/posts/new', function (Request $request) use ($app) {
     //TODO
+    $user_id = intval($request->get('user'));
+    $content = $request->get('content');
+
+    //check if this user exist
+    $sql = "SELECT user_id FROM users WHERE user_id = ?";
+    $user = $app['db']->fetchAssoc($sql, array((int)$user_id));
+    if (!$user) {
+        return $app->json(array('msg' => 'FAIL', 'error' => "User $user_id does not exist."), 400);
+    }
+
+    //check the content is less than 100
+    $str = preg_replace('/\s+/', ' ', $content);
+    if (strlen($str) < 100) {
+        return $app->json(array('msg' => 'FAIL', 'error' => "Content length must be more than 100 characters"), 400);
+    }
+
+    //save the post
+    $insertPost = $app['db']->insert('posts', array('user_id' => $user_id, 'content' => $content, 'date' => strtotime('now')));
+    if (!$insertPost) {
+        return $app->json(array('msg' => 'FAIL', 'error' => "Insertion failed , try again later"), 400);
+    }
+
+    //get the id and return a success response
+    $id = $app['db']->lastInsertId();
+
+    return $app->json(array('msg' => 'OK', 'id' => $id));
+
+
+});
+
+$app->put('/api/posts/edit/{post_id}', function ($post_id, Request $request) use ($app) {
+
+    $user_id = intval($request->get('user'));
+    $content = $request->get('content');
+
+    //check if this blog exist
+    $sql = "SELECT rowid FROM posts WHERE rowid = ?";
+    $user = $app['db']->fetchAssoc($sql, array((int)$post_id));
+    if (!$user) {
+        return $app->json(array('msg' => 'FAIL', 'error' => "Post $post_id does not exist."), 404);
+    }
+
+    //check if this user exist
+    $sql = "SELECT user_id FROM users WHERE user_id = ?";
+    $user = $app['db']->fetchAssoc($sql, array((int)$user_id));
+    if (!$user) {
+        return $app->json(array('msg' => 'FAIL', 'error' => "User $user_id does not exist."), 400);
+    }
+
+    //check the content is less than 100
+    $str = preg_replace('/\s+/', ' ', $content);
+    if (strlen($str) < 100) {
+        return $app->json(array('msg' => 'FAIL', 'error' => "Content length must be more than 100 characters"), 400);
+    }
+
+    //save the post
+    $updatePost = $app['db']->update('posts', array('user_id' => $user_id, 'content' => $content), array('rowid' => (int)$post_id));
+    if (!$updatePost) {
+        return $app->json(array('msg' => 'FAIL', 'error' => "Update failed, try again later"), 400);
+    }
+
+    //return a success response
+    return $app->json(array('msg' => 'OK', 'id' => $post_id));
 });
 
 $app->delete('/api/posts/delete/{id}', function ($id) use ($app) {
+    $id = 15;
     $sql = "SELECT posts.rowid FROM posts  WHERE posts.rowid = ?";
     $post = $app['db']->fetchAssoc($sql, array((int)$id));
 
     if (!$post) {
-        $app->abort(404, "Post $id does not exist.");
+        return $app->json(array('msg' => 'FAIL', 'error' => "Post $id does not exist."), 404);
     }
 
     $sql = "DELETE FROM posts WHERE posts.rowid = ?";
@@ -99,13 +154,7 @@ $app->get('/api/users', function () use ($app) {
 
 
 /* ------- micro-blog web app ---------
-
 All Endpoints for micro-blog web app below.
-
-TODO: Build front-end of web app in the / endpoint below - Add more
-      endpoints if you like.
-
-      See TODO in index.twig for more instructions / suggestions
 */
 
 $app->get('/', function () use ($app) {
@@ -129,7 +178,7 @@ $app->get('/posts/user/{user_id}', function ($user_id) use ($app) {
     ]);
 });
 
-$app->get('/post/id/{post_id}', function ($post_id) use ($app) {
+$app->get('/posts/{post_id}', function ($post_id) use ($app) {
     $sql = "SELECT posts.rowid, posts.*, users.name FROM posts INNER JOIN users ON users.user_id = posts.user_id WHERE posts.rowid = ?";
     $post = $app['db']->fetchAssoc($sql, array((int)$post_id));
 
@@ -144,3 +193,22 @@ $app->get('/post/id/{post_id}', function ($post_id) use ($app) {
 
 
 $app->run();
+
+
+
+/*
+    Add a users table to sqlite db //Done
+
+    Error checking - e.g. if try retrieve posts for a user_id that does not exist,
+        return an error message and an appropriate HTTP status code. //Done
+
+    Implement /api/posts/new endpoint to add a new micro-blog post for a given user. //Done
+
+    Extra: Add new API endpoints for any extra features you can think of. //Done
+
+    Extra: Improve on current API code where you see necessary //Done
+
+    Build front-end of web app in the / endpoint below - Add more endpoints if you like. //Done
+
+    Build the front-end for the micro-blog using a front-end framework of your choice. //Done
+*/
